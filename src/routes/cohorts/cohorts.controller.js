89c8cc439532a52fa2f62e6af7abe5a7a768dc95
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-11-20 13:20:40 -0500

    fix for accumulator weirdness

--- src/routes/cohorts/cohorts.controller.js
+++ src/routes/cohorts/cohorts.controller.js
@@ -1,35 +1,33 @@
-const db = require("../../db");
+const db = require('../../db');
 
 // (stretch todo): write a test for this helper function so that if it
 // gets refactored then we know it still works
 const getCohorts = snapshot => {
-    const cohorts = snapshot.docs.reduce((acc, currentDoc) => {
-        const resolvedDoc = currentDoc.data();
-        if (!acc[resolvedDoc.cohort]) {
-            return { [resolvedDoc.cohort]: 1, ...acc };
-        } else {
-            return { [resolvedDoc.cohort]: acc[resolvedDoc.cohort]++, ...acc };
-        }
-    }, {});
+  const cohorts = snapshot.docs.reduce((acc, currentDoc) => {
+    const resolvedDoc = currentDoc.data();
+    if (!acc[resolvedDoc.cohort]) {
+      return { ...acc, [resolvedDoc.cohort]: 1 };
+    } else {
+      return { ...acc, [resolvedDoc.cohort]: acc[resolvedDoc.cohort] + 1 };
+    }
+  }, {});
 
-    return cohorts;
+  return cohorts;
 };
 
 const cohortsController = (req, res) => {
-    db.collection("application_data")
-        .get()
-        .then(snapshot => {
-            res.json({
-                data: getCohorts(snapshot)
-                })
-        })
-        .catch(error => {
-            res.json({ error });
-        });
-
+  db.collection('application_data')
+    .get()
+    .then(snapshot => {
+      res.json({
+        data: getCohorts(snapshot)
+      });
+    })
+    .catch(error => {
+      res.json({ error });
+    });
 };
 
-  module.exports = {
-    cohortsController
-  }
-  
+module.exports = {
+  cohortsController
+};
