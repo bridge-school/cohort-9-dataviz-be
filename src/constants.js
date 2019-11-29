b883b9929c3d62b2471e277a1a39ac7545109ef0
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-11-29 14:47:10 -0500

    Changing frontend_url back to production netlify url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,3 @@
-const FRONTEND_URL =
-  'https://deploy-preview-12--eager-spence-678935.netlify.com';
+const FRONTEND_URL = 'https://eager-spence-678935.netlify.com';
 
 module.exports = { FRONTEND_URL };
